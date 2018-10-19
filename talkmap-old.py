317af7f9a21a68472e722873e6b8020c0dde40f9
Satoshi YUKAWA <stsykw@gmail.com>
2018-10-19 15:56:40 +0900

    update talkmap generator for manual entries

--- talkmap-old.py
+++ talkmap-old.py
@@ -13,16 +13,11 @@
 
 import glob
 import getorg
-#from geopy import Nominatim
-
-class Coordinate:
-    def __init__(self,lati,long):
-        self.latitude = lati
-        self.longitude = long
+from geopy import Nominatim
 
 g = glob.glob("_talks/*.md")
 
-#geocoder = Nominatim(user_agent="my-talkmapgenerator/1.0")
+geocoder = Nominatim(user_agent="my-talkmapgenerator/1.0")
 location_dict = {}
 location = ""
 permalink = ""
@@ -30,29 +25,17 @@ title = ""
 
 for file in g:
     with open(file, 'r') as f:
-        print(file)
         lines = f.read()
         if lines.find('location: "') > 1:
             loc_start = lines.find('location: "') + 11
             lines_trim = lines[loc_start:]
             loc_end = lines_trim.find('"')
             location = lines_trim[:loc_end]
-        if lines.find('longitude: "') > 1:
-            loc_start = lines.find('longitude: "') + 12
-            lines_trim = lines[loc_start:]
-            loc_end = lines_trim.find('"')
-            longitude = lines_trim[:loc_end]
-        if lines.find('latitude: "') > 1:
-            loc_start = lines.find('latitude: "') + 11
-            lines_trim = lines[loc_start:]
-            loc_end = lines_trim.find('"')
-            latitude = lines_trim[:loc_end]
 
-        location_dict[location] = Coordinate(latitude,longitude)
-        print(location, "\n", location_dict[location].latitude,location_dict[location].longitude)
 
-#for user, loca in location_dict.items():
-    #print(user,loca)
+        location_dict[location] = geocoder.geocode(location)
+        print(location, "\n", location_dict[location])
+
 
 m = getorg.orgmap.create_map_obj()
 getorg.orgmap.output_html_cluster_map(location_dict, folder_name="./talkmap", hashed_usernames=False)
