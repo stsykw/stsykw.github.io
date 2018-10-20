b1ceaa3fd109e23f789f1c4bdf42456c6890c41c
Satoshi YUKAWA <stsykw@gmail.com>
2018-10-20 15:10:53 +0900

    new publication generator

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -1,49 +1,8 @@
 #!/usr/bin/env python3
-# coding: utf-8
-
-# # Publications markdown generator for academicpages
-#
-# Takes a TSV of publications with metadata and converts them for use with [academicpages.github.io](academicpages.github.io). This is an interactive Jupyter notebook, with the core python code in publications.py. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one that fits your format.
-#
-# TODO: Make this work with BibTex and other databases of citations, rather than Stuart's non-standard TSV format and citation style.
-#
-
-# ## Data format
-#
-# The TSV needs to have the following columns: pub_date, title, venue, excerpt, citation, site_url, and paper_url, with a header at the top.
-#
-# - `excerpt` and `paper_url` can be blank, but the others must have values.
-# - `pub_date` must be formatted as YYYY-MM-DD.
-# - `url_slug` will be the descriptive part of the .md file and the permalink URL for the page about the paper. The .md file will be `YYYY-MM-DD-[url_slug].md` and the permalink will be `https://[yourdomain]/publications/YYYY-MM-DD-[url_slug]`
-
-
-# ## Import pandas
-#
-# We are using the very handy pandas library for dataframes.
-
-# In[2]:
 
 import pandas as pd
-
-
-# ## Import TSV
-#
-# Pandas makes this easy with the read_csv function. We are using a TSV, so we specify the separator as a tab, or `\t`.
-#
-# I found it important to put this data in a tab-separated values format, because there are a lot of commas in this kind of data and comma-separated values can get messed up. However, you can modify the import statement, as pandas also has read_excel(), read_json(), and others.
-
-# In[3]:
-
-#publications = pd.read_csv("publications.tsv", sep="\t", header=0)
-publications = pd.read_excel("publications.xlsx",  header=0)
-#publications
-
-
-# ## Escape special characters
-#
-# YAML is very picky about how it takes a valid string, so we are replacing single and double quotes (and ampersands) with their HTML encoded equivilents. This makes them look not so readable in raw format, but they are parsed and rendered nicely.
-
-# In[4]:
+import re
+import urllib.parse as ur
 
 html_escape_table = {
     "&": "&amp;",
@@ -55,59 +14,71 @@ def html_escape(text):
     """Produce entities within text."""
     return "".join(html_escape_table.get(c,c) for c in text)
 
+publications = pd.read_json("publications.json")
 
-# ## Creating the markdown files
-#
-# This is where the heavy lifting is done. This loops through all the rows in the TSV dataframe, then starts to concatentate a big string (```md```) that contains the markdown for each type. It does the YAML metadata first, then does the description for the individual page. If you don't want something to appear (like the "Recommended citation")
+for row, elm in publications.iterrows():
+    item = elm.values[0]
 
-# In[5]:
+    if len(str(item['pub_date'])) > 5:
+        ## make url_slug
+        ## head 20
+        title = re.sub(' +',' ',item['title'])
+        url_slug = ur.quote(title[:20])
 
-import os
-for row, item in publications.iterrows():
-    if len(str(item.pub_date)) > 5:
-        md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
-        html_filename = str(item.pub_date) + "-" + item.url_slug
-        year = item.pub_date[:4]
+        md_filename = str(item['pub_date']) + "-" + url_slug + ".md"
+        html_filename = str(item['pub_date']) + "-" + url_slug
+        year = item['pub_date'][:4]
 
+        author = re.sub(' +',' ',item['author'])
+        venue = re.sub(' +',' ',item['venue'])
         ## YAML variables
 
-        md = "---\ntitle: \""   + item.title + '"\n'
+        md = "---\ntitle: \""   + title + '"\n'
 
         md += """collection: publications"""
 
         md += """\npermalink: /publication/""" + html_filename
 
-        if len(str(item.excerpt)) > 5:
-            md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
+        if 'excerpt' in item:
+            md += "\nexcerpt: '" + html_escape(item['excerpt']) + "'"
 
-        md += "\ndate: " + str(item.pub_date)
+        md += "\ndate: " + str(item['pub_date'])
 
-        md += "\nvenue: '" + html_escape(item.venue) + "'"
+        md += "\nvenue: '" + html_escape(venue) + "'"
 
-        if len(str(item.paper_url)) > 5:
-            md += "\npaperurl: '" + item.paper_url + "'"
+        ## make paper_url fron item['doi']
+        paper_url = ''
+        if 'doi' in item:
+            paper_url += "https://dx.doi.org/" + item['doi']
+            md += "\npaperurl: '" + paper_url + "'"
 
-        md += "\ncitation: '" + html_escape(item.citation) + "'"
+        ## make citations
+        citation ="{}, {}, {}".format(author,title,venue)
+        if 'volume' in item:
+            citation += ", <b>{}</b>".format(item['volume'])
+        if 'start' in item:
+            citation += ", {}".format(item['start'])
+        if 'end' in item:
+            citation += "-{}".format(item['end'])
+        if 'year' in item:
+            citation += ", ({})".format(item['year'])
+        md += "\ncitation: '" + html_escape(citation) + "'"
 
         md += "\n---"
 
         ## Markdown description for individual page
 
-        if len(str(item.author)) > 5:
-            md += "\n\nAuthor(s): " + item.author + "\n"
 
-        if len(str(item.comments)) > 5:
-            md += "\n\nComments: " + item.comments + "\n"
+        md += "\n\nAuthor(s): " + author + "\n"
 
-        if len(str(item.paper_url)) > 5:
-            md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n"
+        if 'comments' in item:
+            md += "\n\nComments: " + item['comments'] + "\n"
 
-        if len(str(item.excerpt)) > 5:
-            md += "\n" + html_escape(item.excerpt) + "\n"
+        if len(str(paper_url)) > 5:
+            md += "\n\n<a href='" + paper_url + "'>Download paper here</a>\n"
 
-        # md += "\nRecommended citation: " + item.citation
+        if 'excerpt' in item:
+            md += "\n" + html_escape(item['excerpt']) + "\n"
 
-        md_filename = os.path.basename(md_filename)
 
-        with open("../_publications/" + md_filename, 'w') as f:
-            f.write(md)
+        print(md)
